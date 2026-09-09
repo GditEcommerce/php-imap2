b44799b0ea50af186a9a95508004a66b3a60f565
Jack <f123248@gmail.com>
2026-09-09 14:08:06 +0800

    Fix `mb_convert_encoding` arguments for UTF-7 functions
    Corrects the `to_encoding` and `from_encoding` parameters in the `utf7Decode`
    and `utf7Encode` methods to ensure proper conversion between UTF-8 and
    UTF7-IMAP. This also fixes a typo in `utf7Decode` from `mb_convert_decoding`
    to the correct `mb_convert_encoding` function.

--- src/Roundcube/ImapClient.php
+++ src/Roundcube/ImapClient.php
@@ -2571,7 +2571,7 @@ class ImapClient
                         $string = preg_replace('/\n[\t\s]*/', ' ', trim($string));
 
                         switch ($field) {
-                            case 'date';
+                            case 'date':
                                 $string                 = substr($string, 0, 128);
                                 $result[$id]->date      = $string;
                                 $result[$id]->timestamp = Utils::strtotime($string);
